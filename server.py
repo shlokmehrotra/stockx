de78558f3ea87e5fbeeb7ddb0c85e6e15f4133d1
Shlok Mehrotra <shlokmehrotra@gmail.com>
2021-02-16 10:39:20 -0800

    stock webscraper first iteration

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 from flask import Flask
-app = Flask(_name_)
+from flask import request, redirect, render_template
+from data_col import price
 
+app = Flask(__name__, template_folder='static')
+stock_name = ""
+price_val = ""
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("testing.html", stock = stock_name, price = price_val)
+@app.route('/checkprice', methods = ['POST', 'GET'])
+def checkprice():
+    stock_name = request.form["stock"]
+    print("REQUEST_DATA" + str(stock_name))
+    print(price(stock_name))
+    price_val = str(price(stock_name))
+    return render_template("testing.html", stock = stock_name, price = price_val)
+if __name__ == "__main__":
+    app.run(debug=True)
