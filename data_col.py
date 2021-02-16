f8d16f244b0148813a3023520e30be4f29e72282
Shlok Mehrotra <shlokmehrotra@gmail.com>
2021-02-15 20:21:34 -0800

    init commits

---
+++ data_col.py
@@ -0,0 +1,14 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+def price(company):
+    url = "https://finance.yahoo.com/quote/" + company + "?p=" + company + "&.tsrc=fin-srch"
+    page = urlopen(url)
+
+    soup = bs4.BeautifulSoup(page,"html.parser")
+
+    price = soup.find("div",{"class": "My(6px) Pos(r) smartphone_Mt(6px)"}).find("span").text
+    return price
