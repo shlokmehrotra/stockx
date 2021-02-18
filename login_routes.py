0de02c694fe9c3e99d7675b9c757e42cd0fafccb
Shlok Mehrotra <shlokmehrotra@gmail.com>
2021-02-18 00:00:04 -0800

    login routes

---
+++ login_routes.py
@@ -0,0 +1,12 @@
+def new_user(user_data):
+    print("New User: ")
+    print(user_data)
+    email = user_data["email"]
+    username = user_data["username"]
+    password = user_data["password"]
+def verify_user(user_data):
+    print("Verify: ")
+    print(user_data)
+    email = user_data["email"]
+    username = user_data["username"]
+    password = user_data["password"]
--- server.py
+++ server.py
@@ -1,12 +1,34 @@
 from flask import Flask
 from flask import request, redirect, render_template
 from API.data_col import price
+from login_routes import new_user, verify_user
 
-app = Flask(__name__, template_folder='static')
-
+#app = Flask(__name__, template_folder='static')
+app = Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("/index.html")
 
-if __name__ == "__main__":
+@app.route("/register")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/user/confirm/<string:decision>", methods = ["POST"])
+#string type is either "login" or "signup"
+def usercheck(decision):
+    print(decision)
+    if(decision == "signup"):
+        new_user(request.form)
+    elif(decision == "login"):
+        verify_user(request.form)
+
+    return redirect("/")
+@app.route("/<string:random>")
+def pageNotFound(random):
+    return("404 Error! Page: " + random + " not found ") 
+
+if __name__ == "__main__": 
     app.run(debug=True)
